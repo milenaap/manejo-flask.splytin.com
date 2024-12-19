3552d950423bc5b7d614f7a22d6c1ee0caa6d2e7
mile <darimile@gmail.com>
2024-12-19 09:42:39 +0100

    add log

--- app.py
+++ app.py
@@ -1,20 +1,58 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, jsonify
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 from cliente import Cliente
 from cliente_dao import ClienteDAO
 
-app = Flask(__name__)
+
 
 titulo_app = 'Zona Fit (GYM) Proxima Estación-'
 
 
-@app.route('/') #url: http://localhost:5000/
-@app.route('/index.html') #url https: //localhost:5000/index.html
+def create_app():
+    app = Flask(__name__)
 
-def inicio():
-    app.logger.debug('Entramos al path de inicio /')
-    clientes_db = ClienteDAO.seleccionar()
-    return render_template('index.html', titulo=titulo_app, clientes=clientes_db)
+    # Crear la carpeta "logs" si no existe
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    # Configurar logging
+    handler = RotatingFileHandler(
+        'logs/app.log', maxBytes=1000000, backupCount=5
+    )  # Archivo de log, 1 MB máx., 5 backups
+    handler.setLevel(logging.ERROR)  # Registrar solo errores y mayores
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(message)s'
+    )  # Formato de los logs
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+
+
+
+    @app.route('/') #url: http://localhost:5000/
+    @app.route('/index.html') #url https: //localhost:5000/index.html
+    def inicio():
+        app.logger.debug('Entramos al path de inicio /')
+        clientes_db = ClienteDAO.seleccionar()
+        return render_template('index.html', titulo=titulo_app, clientes=clientes_db)
+
+    @app.route('/error')
+    def error():
+        v = 1 / 0
+        return jsonify({"mensaje": "Esto nuna se ejecutará"})
+
+    @app.errorhandler(Exception)
+    def handle_exception(e):
+        app.logger.error(f"Error: {str(e)}", exc_info=True)
+        return jsonify({"error": "Error interno del servidor"}), 500
+
+    return app
+
+
+
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
+
